3e4bde9928d92b7b91c8256a29e64891f1aaff0b
JuanR547 <95152166+JuanR547@users.noreply.github.com>
2023-05-14 12:17:25 -0300

    frontend

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { InformacionPersonalComponent } from './components/informacion-personal/informacion-personal.component';
+import { ExperienciasComponent } from './components/experiencias/experiencias.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    BannerComponent,
+    InformacionPersonalComponent,
+    ExperienciasComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
